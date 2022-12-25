19929086c867b72fff5a2bcd27a9c353aebb6a3a
moo3oo3oo3 <moo3oo3oo3@gmail.com>
2022-12-24 21:28:37 -0600

    Create songDirectory.lua

---
+++ CC_Tweaked/songDirectory.lua
@@ -0,0 +1,7 @@
+{
+	["Fallen Kingdom"] = 'https://github.com/moo3oo3oo3/moo3oo3oo3.github.io/raw/master/CC_Tweaked/fallen_kingdom.dfpwm',
+	["Take Back the Night"] = 'https://github.com/moo3oo3oo3/moo3oo3oo3.github.io/raw/master/CC_Tweaked/take_back_the_night.dfpwm',
+	["Find the Pieces"] = 'https://github.com/moo3oo3oo3/moo3oo3oo3.github.io/raw/master/CC_Tweaked/find_the_pieces.dfpwm',
+	["Dragonhearted"] = 'https://github.com/moo3oo3oo3/moo3oo3oo3.github.io/raw/master/CC_Tweaked/dragonhearted.dfpwm',
+	["Rising Kingdom"] = 'https://github.com/moo3oo3oo3/moo3oo3oo3.github.io/raw/master/CC_Tweaked/rising_kingdom.dfpwm'
+}
