a59af6fc30c61b748c0a0fb0b0a9277f80565d8c
moo3oo3oo3 <moo3oo3oo3@gmail.com>
2022-12-29 17:53:05 -0600

    Update files.lua

--- CC_Tweaked/iMoo/files.lua
+++ CC_Tweaked/iMoo/files.lua
@@ -5,6 +5,6 @@
 	['notes.bimg'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/notes.bimg',
 	['iMooUtils.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/iMooUtils.lua',
 	['startup.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/startup.lua',
-	['colonyRecruiter'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/colonyRecruiter.lua',
+	['colonyRecruiter.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/colonyRecruiter.lua',
 	['musicPlayer.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/musicPlayer.lua',
 }
