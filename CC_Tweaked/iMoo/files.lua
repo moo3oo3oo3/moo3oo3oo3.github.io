5bcc68f913885130946e625c41f052cc1c6cf94b
moo3oo3oo3 <moo3oo3oo3@gmail.com>
2022-12-29 17:52:35 -0600

    Update files.lua

--- CC_Tweaked/iMoo/files.lua
+++ CC_Tweaked/iMoo/files.lua
@@ -1,8 +1,10 @@
 {
 	['audioPlayer.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/audioPlayer.lua',
 	['red_question_mark.bimg'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/red_question_mark.bimg',
+	['colonyCompass.bimg'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/colonyCompass.bimg',
 	['notes.bimg'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/notes.bimg',
 	['iMooUtils.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/iMooUtils.lua',
 	['startup.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/startup.lua',
+	['colonyRecruiter'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/colonyRecruiter.lua',
 	['musicPlayer.lua'] = 'https://raw.githubusercontent.com/moo3oo3oo3/moo3oo3oo3.github.io/master/CC_Tweaked/iMoo/musicPlayer.lua',
 }
